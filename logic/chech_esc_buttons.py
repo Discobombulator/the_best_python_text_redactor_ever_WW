2f63467a7f08b436e1666f15a8ae3c3762648e19
Yaroslav Ozornin <Runboll023@gmail.com>
2025-04-11 14:18:28 +0500

    пара коментов

--- logic/chech_esc_buttons.py
+++ logic/chech_esc_buttons.py
@@ -5,14 +5,19 @@ from visual.exit_and_save_scene import exit_no_save
 
 def is_program_end(std, text_type, text, name):
     end_type = main_controller(std)
+
+    #Выход без сохранения
     if end_type == "exit_no_save":
         if exit_no_save(std) == "exit_no_save":
             return "exit"
+    #Сохранение с новым названием
     elif end_type == "save_how":
         save_text_no_name(std, text)
+    # Сохранение по старому названию
     elif end_type == "save":
         if text_type == "olf_f":
             save_text(text, name)
+    # Сохранение с новым названием и выход
     elif end_type == "exit_save":
         save_text_no_name(std, text)
         return "exit"
